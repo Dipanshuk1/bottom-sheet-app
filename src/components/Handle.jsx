9f8833befd2be9d3fe3d81826a2051dbe4b0611a
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-07-21 15:34:53 +0530

    Added Handle.jsx

---
+++ src/components/Handle.jsx
@@ -0,0 +1,10 @@
+// components/Handle.jsx
+import React from 'react';
+
+export default function Handle() {
+  return (
+    <div className="w-full flex justify-center py-2 cursor-pointer">
+      <div className="w-12 h-1.5 bg-gray-300 rounded-full"></div>
+    </div>
+  );
+}
