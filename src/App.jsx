75475c0d21f881320096e3f22d432722f26a0181
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-07-21 15:13:24 +0530

    Updated App.jsx and used BottomSheet component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+// src/App.jsx
+import React from "react";
+import BottomSheet from "./components/BottomSheet";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="App">
+      <h1>React Bottom Sheet</h1>
+      <p>Click buttons or drag to move the sheet.</p>
+      <BottomSheet />
+    </div>
+  );
 }
 
-export default App
+export default App;
